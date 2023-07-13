8ee45d4aab0130acc074c540b293b64577828f19
devbok0101 <devbok0101@gmail.com>
2023-07-14 00:20:52 +0900

    [ADD] 회원 save, find 기능 추가

---
+++ src/main/java/jpabook/jpashop/test/entity/Member.java
@@ -0,0 +1,17 @@
+package jpabook.jpashop.test.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import lombok.Getter;
+
+@Entity
+@Getter
+public class Member {
+    @Id
+    @GeneratedValue
+    private Long id;
+    private String name;
+
+
+}
