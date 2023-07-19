80623ec77c0f132b9c341aea5658bc6ced0e72dc
devbok0101 <devbok0101@gmail.com>
2023-07-19 18:31:04 +0900

    [ADD] Category 도메인 추가 완료

--- src/main/java/jpabook/jpashop/domain/item/Item.java
+++ src/main/java/jpabook/jpashop/domain/item/Item.java
@@ -3,6 +3,9 @@ package jpabook.jpashop.domain.item;
 import jakarta.persistence.*;
 import lombok.Getter;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Entity
 @Getter
 @Inheritance(strategy = InheritanceType.SINGLE_TABLE)
@@ -17,4 +20,7 @@ public abstract class Item {
     private String name;
     private int price;
     private int stockQuantity;
+
+    @ManyToMany(mappedBy = "items")
+    private List<Category> categories = new ArrayList<>();
 }
