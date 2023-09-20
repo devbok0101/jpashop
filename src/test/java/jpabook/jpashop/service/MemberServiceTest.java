06eb647a4fd9ee406b889ba593d81be8eacea501
devbok0101 <devbok0101@gmail.com>
2023-09-21 00:13:00 +0900

    [ADD] 테스트 코드 메모리 db 사용하도록 변경

--- src/test/java/jpabook/jpashop/service/MemberServiceTest.java
+++ src/test/java/jpabook/jpashop/service/MemberServiceTest.java
@@ -5,7 +5,6 @@ import jpabook.jpashop.repository.MemberRepository;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.annotation.Rollback;
 import org.springframework.transaction.annotation.Transactional;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -37,7 +36,5 @@ class MemberServiceTest {
 
         memberService.join(member1);
         assertThrows(IllegalStateException.class, () -> memberService.join(member2));
-
-        //fail("예외가 발생해야합니다.");
     }
 }
